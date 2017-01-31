7929c03797e7b7f2df6e872014aedd0d6fc3c24a
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2017-01-31 17:11:45 +0100

    Fix connection leak

--- server/main.go
+++ server/main.go
@@ -48,6 +48,8 @@ type Packet struct {
 func main() {
 	writer := make(chan Packet)
 	send := make(chan string)
+	logger := time.NewTicker(10 * time.Second).C
+	go Log(logger)
 	readAIS(send)
 	go ReadHttp("ECC", "http://aishub.ais.ecc.no/raw", 5*time.Second, writer)
 	go ReadTCP("Kystverket", "153.44.253.27:5631", 5*time.Second, writer)
@@ -67,29 +69,41 @@ func WatchBuffer(buffer chan Packet) {
 
 }
 
+func Log(ticker <-chan time.Time) {
+	for _ = range ticker {
+		fmt.Println("source connections: ", connections)
+	}
+}
+
+var connections = 0
+
 func ReadTCP(name string, ip string, silence_timeout time.Duration, writer chan Packet) {
 	for {
-		addr, err := net.ResolveTCPAddr("tcp", ip)
-		CheckErr(err, "Resolve tcp address")
-		conn, err := net.DialTCP("tcp", nil, addr)
-		CheckErr(err, "listen to tcp")
-		defer conn.Close() // FIXME can fail
-		//conn.CloseWrite()
-		buf := make([]byte, 4096)
-		for {
-			conn.SetReadDeadline(time.Now().Add(silence_timeout))
-			n, err := conn.Read(buf)
-			if err != nil {
-				log.Printf("\n\n\n%s read error: %s\n", name, err.Error())
-				break
-			} else {
-				writer <- Packet{
-					source:   name,
-					received: time.Now(),
-					data:     buf[0:n],
+		func() { // scope for the defers
+			connections++
+			defer func() { connections-- }()
+			addr, err := net.ResolveTCPAddr("tcp", ip)
+			CheckErr(err, "Resolve tcp address")
+			conn, err := net.DialTCP("tcp", nil, addr)
+			CheckErr(err, "listen to tcp")
+			defer conn.Close() // FIXME can fail
+			//conn.CloseWrite()
+			buf := make([]byte, 4096)
+			for {
+				conn.SetReadDeadline(time.Now().Add(silence_timeout))
+				n, err := conn.Read(buf)
+				if err != nil {
+					log.Printf("\n\n\n%s read error: %s\n", name, err.Error())
+					break
+				} else {
+					writer <- Packet{
+						source:   name,
+						received: time.Now(),
+						data:     buf[0:n],
+					}
 				}
 			}
-		}
+		}()
 	}
 }
 
@@ -103,32 +117,36 @@ func ReadHttp(name string, url string, silence_timeout time.Duration, writer cha
 		Timeout:       0,   // Counts from start of connection
 	}
 	for {
-		request, err := http.NewRequest("GET", url, nil)
-		CheckErr(err, "Create request")
-		resp, err := client.Do(request)
-		CheckErr(err, fmt.Sprintf("connect to %ss receiver", name))
-		defer resp.Body.Close()
-		// Body is only ReadCloser, and GzipReader isn't Conn so type asserting won't work.
-		// If it did we could set its timeout directly
-		// We could also check and branch to two different implementations.
-		// if resp.Body.(net.Conn) != nil {
-		// 	fmt.Printf("http.Response.Body is a %T\n", resp.Body)
-		// }
+		func() { // scope for the defers
+			connections++
+			defer func() { connections-- }()
+			request, err := http.NewRequest("GET", url, nil)
+			CheckErr(err, "Create request")
+			resp, err := client.Do(request)
+			CheckErr(err, fmt.Sprintf("connect to %ss receiver", name))
+			defer resp.Body.Close()
+			// Body is only ReadCloser, and GzipReader isn't Conn so type asserting won't work.
+			// If it did we could set its timeout directly
+			// We could also check and branch to two different implementations.
+			// if resp.Body.(net.Conn) != nil {
+			// 	fmt.Printf("http.Response.Body is a %T\n", resp.Body)
+			// }
 
-		buf := make([]byte, 4096)
-		for {
-			n, err := resp.Body.Read(buf)
-			if err != nil {
-				log.Printf("\n\n\n%s read error: %s\n", name, err.Error())
-				break
-			} else {
-				writer <- Packet{
-					source:   name,
-					received: time.Now(),
-					data:     buf[0:n],
+			buf := make([]byte, 4096)
+			for {
+				n, err := resp.Body.Read(buf)
+				if err != nil {
+					log.Printf("\n\n\n%s read error: %s\n", name, err.Error())
+					break
+				} else {
+					writer <- Packet{
+						source:   name,
+						received: time.Now(),
+						data:     buf[0:n],
+					}
 				}
 			}
-		}
+		}()
 	}
 }
 
